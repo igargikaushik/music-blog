4e849b0e7c91a24186cd337597a1c1c623f38486
Henry Sloan <henryksloan@gmail.com>
2020-12-02 17:34:03 -0500

    Add placeholder tags to tiles

--- src/components/tiles/Tile.vue
+++ src/components/tiles/Tile.vue
@@ -4,7 +4,8 @@
       <p class="is-size-5 title has-text-left">
         {{ title }}
       </p>
-      <article v-if="imgSrc != null" class="media">
+
+      <article v-if="imgSrc != null" class="media mb-1">
         <figure class="media-left">
           <p class="image is-128x128">
             <img :src="imgSrc" />
@@ -15,26 +16,54 @@
         </section>
       </article>
 
-      <section v-else class="has-text-left">
+      <section v-else class="has-text-left mb-1">
         {{ content }}
       </section>
+
+      <div id="tags" class="field is-grouped is-grouped-multiline">
+        <div class="control">
+          <div class="tags has-addons">
+            <a class="tag is-link">Article</a>
+          </div>
+        </div>
+        <div class="control">
+          <div class="tags has-addons">
+            <a class="tag is-light">Sonata</a>
+          </div>
+        </div>
+        <div class="control">
+          <div class="tags has-addons">
+            <a class="tag is-light">Classical</a>
+          </div>
+        </div>
+      </div>
     </article>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Tile',
+  name: "Tile",
   props: {
     title: String,
-    imgSrc: { type: String, default: null},
-    content: String
-  }
-}
+    imgSrc: { type: String, default: null },
+    content: String,
+  },
+};
 </script>
 
 <style scoped lang="scss">
 @import "~bulma/sass/utilities/_all";
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
+
+.box {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+#tags {
+  margin-top: auto;
+}
 </style>
