9320287b39a316fc46dd40794f0e91303f72f07f
Henry Sloan <henryksloan@gmail.com>
2020-12-04 15:54:59 -0500

    Make article use markdown with placeholder text

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -13,17 +13,10 @@
           <div class="column"></div>
         </div>
         <div class="columns">
-          <div class="column has-text-left" id="contents">
-            <strong>Contents:</strong>
-            <ol>
-              <li><a>Sonata vs. sonata form</a></li>
-              <li><a>History of the sonata</a></li>
-              <li><a>Suggestions</a></li>
-            </ol>
-          </div>
+          <div class="column"></div>
           <div class="column is-three-quarters has-text-left">
-            <div class="content" v-html="content"></div>
-
+            <!--markdown-content class="content" /!-->
+            <markdown-it-vue class="content md-body" ref="markdownIt" :content="content" :options="options" />
             <div class="columns" id="media-tags">
               <div class="column is-one-third">
                 <div class="box">
@@ -87,6 +80,16 @@
 
 <script>
 import axios from 'axios';
+/* import MarkdownContent from 'D:/Henry/dev/music_blog/test.md'
+
+import Tile from '@/components/tiles/Tile.vue'
+MarkdownContent.vue.component.components = {
+  Tile
+} */
+import MarkdownItVue from 'markdown-it-vue'
+// import 'markdown-it-vue/dist/markdown-it-vue.css'
+const markdownItClass = require('@toycode/markdown-it-class')
+const markdownItToc = require('markdown-it-table-of-contents')
 
 export default {
   name: 'Home',
@@ -96,11 +99,30 @@ export default {
       test: "",
       author: "Henry Sloan",
       description: "Many of the most popular classical works are \"Sonatas\". Let's look at what that means, and how we can navigate this vast genre.",
-      content: "<p>&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;Hello, world! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><p>&nbsp;&nbsp;&nbsp;&nbspOne morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p><p>&nbsp;&nbsp;&nbsp;&nbspHe lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</p><p>&nbsp;&nbsp;&nbsp;&nbspThe bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.</p><p>&nbsp;&nbsp;&nbsp;&nbsp'What's happened to me? ' he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls.</p><p>&nbsp;&nbsp;&nbsp;&nbspA collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.</p><p>&nbsp;&nbsp;&nbsp;&nbspIt showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p><p>&nbsp;&nbsp;&nbsp;&nbspDrops of rain could be heard hitting the pane, which made him feel quite sad.</p><p>&nbsp;&nbsp;&nbsp;&nbsp'How about if I sleep a little bit longer and forget all this nonsense', he thought, but that was something he was unable to do because he was used to sleeping on his right, and in his present state couldn't get into that position.</p><p>&nbsp;&nbsp;&nbsp;&nbspHowever hard he threw himself onto his right, he always rolled back to where he was.</p><p>&nbsp;&nbsp;&nbsp;&nbspHe must have tried it a hundred times, shut his eyes so that he wouldn't have to look at the floundering legs, and only stopped when he began to feel a mild, dull pain there that he had never felt before. 'Oh, God', he thought, 'what a strenuous career it is that I've chosen!</p><p>&nbsp;&nbsp;&nbsp;&nbspTravelling day in and day out.</p><p>&nbsp;&nbsp;&nbsp;&nbspDoing business like this takes much more effort than doing your own business at home, and on top of that there's the curse of travelling, worries about making train connections, bad and irregular food, contact with different people all the time so that you can never get to know anyone or become friendly with them. It can all go to Hell!</p><p>&nbsp;&nbsp;&nbsp;&nbsp' He felt a slight itch up on his belly; pushed himself slowly up on his back towards the headboard so that he could lift his head better; found where the itch was, and saw that it was covered with lots of little white spots which he didn't know what to make of; and when he tried to feel the place with one of his legs he drew it quickly back because as soon as he touched it he was overcome by a cold shudder. He slid back into his former position. 'Getting up early all the time', he thought, 'it makes you stupid. You've got</p>"
+      contentb: "<p>&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;Hello, world! Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><p>&nbsp;&nbsp;&nbsp;&nbspOne morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.</p><p>&nbsp;&nbsp;&nbsp;&nbspHe lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.</p><p>&nbsp;&nbsp;&nbsp;&nbspThe bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.</p><p>&nbsp;&nbsp;&nbsp;&nbsp'What's happened to me? ' he thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls.</p><p>&nbsp;&nbsp;&nbsp;&nbspA collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.</p><p>&nbsp;&nbsp;&nbsp;&nbspIt showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p><p>&nbsp;&nbsp;&nbsp;&nbspDrops of rain could be heard hitting the pane, which made him feel quite sad.</p><p>&nbsp;&nbsp;&nbsp;&nbsp'How about if I sleep a little bit longer and forget all this nonsense', he thought, but that was something he was unable to do because he was used to sleeping on his right, and in his present state couldn't get into that position.</p><p>&nbsp;&nbsp;&nbsp;&nbspHowever hard he threw himself onto his right, he always rolled back to where he was.</p><p>&nbsp;&nbsp;&nbsp;&nbspHe must have tried it a hundred times, shut his eyes so that he wouldn't have to look at the floundering legs, and only stopped when he began to feel a mild, dull pain there that he had never felt before. 'Oh, God', he thought, 'what a strenuous career it is that I've chosen!</p><p>&nbsp;&nbsp;&nbsp;&nbspTravelling day in and day out.</p><p>&nbsp;&nbsp;&nbsp;&nbspDoing business like this takes much more effort than doing your own business at home, and on top of that there's the curse of travelling, worries about making train connections, bad and irregular food, contact with different people all the time so that you can never get to know anyone or become friendly with them. It can all go to Hell!</p><p>&nbsp;&nbsp;&nbsp;&nbsp' He felt a slight itch up on his belly; pushed himself slowly up on his back towards the headboard so that he could lift his head better; found where the itch was, and saw that it was covered with lots of little white spots which he didn't know what to make of; and when he tried to feel the place with one of his legs he drew it quickly back because as soon as he touched it he was overcome by a cold shudder. He slid back into his former position. 'Getting up early all the time', he thought, 'it makes you stupid. You've got</p>",
+      options: { markdownIt: { html: true } },
+      content: `
+[[toc]]
+
+# Sonata vs. Sonata Form
+What is the difference? There is a difference. What is it. It is.
+
+# History of the Sonata
+There is history. What is it? It is.
+
+# Suggestions
+Listen! To what? Well, listen, and I'll tell you! It is.
+`
     }
   },
   created() {
     this.fetchTest();
+  },
+  mounted() {
+    const mapping = { h1: 'title' };
+    const toc_options = { containerClass: 'box markdown-toc', containerHeaderHtml: '<strong>Contents</strong>', listType: 'ol' };
+    this.$refs.markdownIt.use(markdownItClass, mapping);
+    this.$refs.markdownIt.use(markdownItToc, toc_options);
   },
   watch: {
     '$route': 'fetchTest'
@@ -113,9 +135,25 @@ export default {
         this.test = response.data;
       });
     }
+  },
+  components: {
+    MarkdownItVue 
+    // MarkdownContent: MarkdownContent.vue.component
   }
 }
 </script>
+
+<style lang="scss">
+// Unscoped styles for markdown
+.markdown-toc ol {
+  margin: 0;
+  padding-left: 16px; 
+}
+
+.markdown-body p:empty {
+  display: none;
+}
+</style>
 
 <style scoped lang="scss">
 @import "~bulma/sass/utilities/_all";
@@ -129,35 +167,24 @@ export default {
   padding-right: 8px;
 }
 
-#contents {
-  a {
-      text-decoration: none;
-      display: block;
-      width: 100%;
-      height: 100%;
-  }
-}
-
 #media-tags {
   .column {
     display: flex;
     align-items: stretch;
     flex-flow: column;
+    min-width: 250px;
   }
 
   .box {
     height: 100%;
     vertical-align: middle;
+    padding-bottom: 4px;
   }
 }
 
 @media screen and (max-width: $tablet) {
   .column:empty {
     display: none;
-  }
-
-  #contents ol {
-    padding-left: 16px;
   }
 }
 </style>
