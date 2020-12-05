30e04f92cd609facf6aac2038c06483e79fa7170
Henry Sloan <henryksloan@gmail.com>
2020-12-05 14:45:42 -0500

    Update placeholder API and remove old files

--- api/index.js
+++ api/index.js
@@ -4,21 +4,10 @@ const cors = require("cors");
 
 const app = express();
 
-/* var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions)); */
-
-// parse requests of content-type - application/json
 app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Root path." });
 });
 
 app.get("/api", (req, res) => {
@@ -29,8 +18,7 @@ app.get("/api/", (req, res) => {
   res.json({ message: "This is the OTHER API GET." });
 });
 
-// set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
