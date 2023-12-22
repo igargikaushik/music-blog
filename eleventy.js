29a184ef02b444376aee8766d4d754d2c4183045
igargikaushik <133994504+igargikaushik@users.noreply.github.com>
2023-12-22 18:31:16 +0530

    eleventy.js

---
+++ eleventy.js
@@ -0,0 +1,37 @@
+/* global require module */
+const pluginNavigation = require('11ty/eleventy-navigation')
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginNavigation)
+
+  eleventyConfig.setDataDeepMerge(true)
+
+  eleventyConfig.addLayoutAlias('activity', 'activity.html.njk')
+
+
+//https://github.com/igargikaushik/music-blog/new/main
+  eleventyConfig.setBrowserSyncConfig({
+    
+  })
+
+  require('./src/_11ty/filters').addFilters(eleventyConfig)
+  require('./src/_11ty/shortcodes').addShortcodes(eleventyConfig)
+  require('./src/_11ty/collections').addCollections(eleventyConfig)
+  require('./src/_11ty/markdown').configureMarkdown(eleventyConfig)
+  require('./src/_11ty/passthrough').passthroughCopy(eleventyConfig)
+
+  return {
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+
+    dir: {
+      input: 'pages',
+      includes: '../src/_includes',
+      layouts: '../src/_layouts',
+      data: '../src/_data',
+      output: '_site',
+    },
+  }
+}
